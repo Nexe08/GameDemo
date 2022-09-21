36e44f546f6fe0ab517b9abc0f5477d1d0a9c1fc
Nexe08 <rohitrautela1408@gmail.com>
2022-09-22 01:15:12 +0530

    Implement character jump logic.

--- SuperCreateForce/Character/character.cs
+++ SuperCreateForce/Character/character.cs
@@ -7,22 +7,36 @@ public class character : KinematicBody2D
     [Export]
     private float gravity = 900.0f;
     [Export]
+    private float jumpForce = -300f;
+    [Export]
     private float movementSpeed = 400;
 
     private Vector2 velocity = Vector2.Zero;
     private Vector2 UP = Vector2.Up;
 
 
+    public override void _Ready()
+    { }
+
+
     public override void _PhysicsProcess(float delta)
     {
+        // floorDetector.ForceRaycastUpdate();
         PlayerMovement(delta);
         velocity.y += gravity * delta;
         velocity = MoveAndSlide(velocity, UP);
     }
 
+
     private void PlayerMovement(float delta)
     {
-        int HDirection = (int) (Input.GetActionStrength("Right") - Input.GetActionStrength("Left"));
+        // Horizontal movement
+        int HDirection = (int)(Input.GetActionStrength("Right") - Input.GetActionStrength("Left"));
         velocity.x = movementSpeed * HDirection;
+        // Vertical movement (jump)
+        if (Input.IsActionJustPressed("Up") && IsOnFloor())
+        {
+            velocity.y = jumpForce;
+        }
     }
 }
